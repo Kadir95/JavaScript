665d2a44e020ef4db97be39e002885ed093f568a
Kadir <kadiralibasic123@gmail.com>
2021-05-25 21:49:36 +0200

    Prvi commit- uvod

---
+++ main.js
@@ -0,0 +1,7 @@
+// uvod //
+// alert('nesto') -daje neki pop-up kad se otvori stranica; blokira scriptu da radi, neefikasna je
+// console.log('Zdravo svijete'); - bolji od alerta, izbacuje pop-up
+// console.error('Ovo je neki error'); - pokazuje na neki error
+// console.warning('neko upozorenje'); - daje neko upozorenje
+
+// Varijable //
